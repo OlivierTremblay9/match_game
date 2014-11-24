97dd8b816255dd0104593d86319c5c0f0c1c9668
OlivierTremblay9 <oli_0007@hotmail.com>
2014-11-24 15:29:26 -0500

    second draft

--- spirals/s01/bin/board.dart
+++ spirals/s01/bin/board.dart
@@ -12,7 +12,6 @@ class Board {
     context = canvas.getContext('2d');
     width = canvas.width;
     height = canvas.height;
-    context.rect(10,10,200,100);
     window.animationFrame.then(gameLoop);
   }
 
@@ -23,15 +22,35 @@ class Board {
 
   void clear() {
     context.clearRect(0, 0, width, height);
+    
   }
 
   void border() {
-
-    context.beginPath();
-    context.fillStyle = 'lightBlue';
-    context.rect(x, y, width, height);
-    context.fillRect(x, y, width, height);
-    context.closePath();
+    var grd = context.createLinearGradient(0, 0, canvas.width, canvas.height);
+    context.rect(0, 0, width, height);
+    grd.addColorStop(0, '#8ed6ff');
+    grd.addColorStop(1, '#004cb3');
+    context.fillStyle = grd;
+    context.fillStyle = 'grd';
+    context.fill();
+    context.moveTo(0, 27); //point 1.1 
+    context.lineTo(650, 27);//point 1.2 
+    context.moveTo(0, 176); //point 2.1 
+    context.lineTo(650, 176);//point 2.2
+    context.moveTo(0, 325); //point 3.1 
+    context.lineTo(650, 325);//point 3.2
+    context.moveTo(27, 0); //point 4.1 
+    context.lineTo(27, 352);//point 4.2
+    context.moveTo(176, 0); //point 8.1 
+    context.lineTo(176, 352);//point 8.2
+    context.moveTo(325, 0); //point 5.1 
+    context.lineTo(325, 352);//point 5.2
+    context.moveTo(474, 0); //point 6.1 
+    context.lineTo(474, 352);//point 6.2
+    context.moveTo(623, 0); //point 7.1 
+    context.lineTo(623, 352);//point 7.2
+    context.lineWidth = 2;
+    context.strokeStyle = 'black';
     context.stroke();
   }
   
@@ -40,3 +59,4 @@ class Board {
     border();
   }
 }
+
